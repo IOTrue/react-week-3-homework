fd8eeff83b83b858cf31df956e127916a05fa571
IOTrue <kjoonc.dev@gmail.com>
2023-01-12 09:55:15 +0900

    업데이트

--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios"
-import { isDev, serverUrl } from ".";
+import { serverUrl } from ".";
 
 
 const initialState = {
